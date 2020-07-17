98ecd565bd1b506ebe4b7b80bf392e842625184f
mohanK22 <mohankadu22@gmail.com>
2020-07-17 21:26:43 +0530

    ActivityBuildersModule Created

--- app/src/main/java/com/example/android/daggerdemo/AuthActivity.kt
+++ app/src/main/java/com/example/android/daggerdemo/AuthActivity.kt
@@ -1,11 +1,19 @@
 package com.example.android.daggerdemo
 
 import android.os.Bundle
+import android.util.Log
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
 class AuthActivity : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var someString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        Log.i("AuthActivity:onCreate()", someString)
     }
 }
