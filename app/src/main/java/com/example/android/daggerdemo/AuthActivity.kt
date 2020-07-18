b56cdb69e3c27b68fb34b654925e87985f2a90fb
mohanK22 <mohankadu22@gmail.com>
2020-07-18 13:16:22 +0530

    Image loading code written

--- app/src/main/java/com/example/android/daggerdemo/AuthActivity.kt
+++ app/src/main/java/com/example/android/daggerdemo/AuthActivity.kt
@@ -1,19 +1,34 @@
 package com.example.android.daggerdemo
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.RequestManager
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var someString: String
 
+    @Inject
+    lateinit var appLogo: Drawable
+
+    @Inject
+    lateinit var requestManager: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-
         Log.i("AuthActivity:onCreate()", someString)
+        setLogo()
+    }
+
+    private fun setLogo() {
+        requestManager
+            .load(appLogo)
+            .into(imgLoginLogo)
     }
 }
--- app/src/main/java/com/example/android/daggerdemo/di/AppModule.kt
+++ app/src/main/java/com/example/android/daggerdemo/di/AppModule.kt
@@ -1,5 +1,12 @@
 package com.example.android.daggerdemo.di
 
+import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+import com.example.android.daggerdemo.R
 import dagger.Module
 import dagger.Provides
 
@@ -10,6 +17,28 @@ class AppModule {
         @Provides
         fun someString(): String {
             return "Some String Test"
+        }
+
+        @Provides
+        fun provideRequestOptions(): RequestOptions {
+            return RequestOptions
+                .placeholderOf(R.drawable.white_background)
+                .error(R.drawable.white_background)
+        }
+
+        @Provides
+        fun provideGlideInstance(
+            application: Application,
+            requestOptions: RequestOptions
+        ): RequestManager {
+            return Glide
+                .with(application)
+                .setDefaultRequestOptions(requestOptions)
+        }
+
+        @Provides
+        fun provideAppLogo(application: Application): Drawable {
+            return ContextCompat.getDrawable(application, R.drawable.dagger_logo)!!
         }
     }
 }
